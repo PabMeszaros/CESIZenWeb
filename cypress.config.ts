c6c5db106804d5559b589fab1bddcfd9bd0b2047
PabMeszaros <pabmeszaros@gmail.com>
2025-09-03 13:39:08 +0200

    demo

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080', // ou 8081 si Docker
     setupNodeEvents(on, config) {
-      // On ne met rien ici, pas de ts-loader
+      // On ne met rien ici, pas de ts-loaderHKKJ
     },
   },
 })
